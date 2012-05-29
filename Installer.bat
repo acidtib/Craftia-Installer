e8b64f6585c066cfa1c87de9299c83c7477104d8
Dainel Vera <dainel@kurubit.com>
2012-05-29 11:38:08 -0400

    initial commit

---
+++ Installer.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+
+:: Simple describtion
+ECHO This will make this installation of all mods and changes to jar files. 
+
+PAUSE
+
+:: Download zip file
+wget.exe http://craftia.kurubit.com/testing/modss.zip
+
+PAUSE
