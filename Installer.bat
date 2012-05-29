641c54cf0ad9bd28dbf4accfa21e99887f3d1e1b
Dainel Vera <dainel@kurubit.com>
2012-05-29 12:31:10 -0400

    move to directory

--- Installer.bat
+++ Installer.bat
@@ -1,11 +1,15 @@
-ECHO OFF
+ECHO ON
 
 :: Simple describtion
 ECHO This will make this installation of all mods and changes to jar files. 
 
 PAUSE
 
+:: Move to %appdata%/.minecraft/bin
+cd %appdata%/.minecraft/bin
+
+
 :: Download zip file
-wget.exe http://craftia.kurubit.com/testing/modss.zip
+::wget.exe http://craftia.kurubit.com/installer/installer.zip
 
 PAUSE
